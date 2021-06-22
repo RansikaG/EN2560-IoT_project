7276814fd31dc870d05d205e4e71643cc1051b16
Ransika <ransika1998@gmail.com>
2021-06-22 19:32:19 +0530

    Using client
    request decoding failed

---
+++ WebServer_XML/WebServer_XML.ino
@@ -0,0 +1,64 @@
+//============================================
+//ESP8266 Web Server (HTML + CSS + JavaScript)
+//============================================
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "webpage.h";
+//------------------------------------------
+ESP8266WebServer server(80);
+//WiFiServer server(80);
+const char* ssid = "RANSIKA";
+const char* password = "RWIFI1234";
+//------------------------------------------
+void webpage()
+{
+  server.send(200,"text/html", webpageCode);
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+void location() { //Handler for the body path
+      if (server.hasArg("country")== false){ //Check if body received
+ 
+            server.send(200, "text/plain", "Body not received");
+            return;
+ 
+      }
+ 
+      String message = "Body received:\n";
+             message += server.arg("country");
+             message += "\n";
+ 
+      server.send(200, "text/plain", message);
+      Serial.println(message);
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=++++++
+//=================================================================
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status()!=WL_CONNECTED){delay(500);Serial.print(".");}
+  Serial.println();
+  Serial.print("IP Address: "); Serial.println(WiFi.localIP());
+  server.on("/", webpage);
+  //server.on("/location",location);
+  server.begin();
+}
+//=================================================================
+void loop()
+{
+  server.handleClient();
+  WiFiClient client = server.client();
+  if (!client) {
+   return;
+  }
+  Serial.println(F("new client"));
+
+  client.setTimeout(1000); // default is 1000
+
+  // Read the first line of the request
+  String req = String(client.read());
+  Serial.println(F("request: "));
+  Serial.println(req);
+
+  
+}
