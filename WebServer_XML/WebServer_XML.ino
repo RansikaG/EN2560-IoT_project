114b8959f8c73d7e6310bb0c2a62f454ab896ff8
Ransika <ransika1998@gmail.com>
2021-06-25 13:42:40 +0530

    Server and node red connected succesfully

--- WebServer_XML/WebServer_XML.ino
+++ WebServer_XML/WebServer_XML.ino
@@ -3,21 +3,95 @@
 //============================================
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <PubSubClient.h>
 #include "webpage.h";
 //------------------------------------------
 ESP8266WebServer server(80);
-
+WiFiClient espClient;
+PubSubClient client(espClient);
 const char* ssid = "RANSIKA";
 const char* password = "RWIFI1234";
+
+const char* mqtt_server = "test.mosquitto.org";
+const char* outTopic = "ENTC/EN2560/out/180241M";
+const char* inTopic = "ENTC/EN2560/in/180241M";
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
 int n=0;
 String temperature="90";
 String locset="True";
 String country="Sri Lanka";
 String city="Colombo";
+String country_nodered="SSSS";
+String city_nodered="SSDDDd";
 String temp="30";
 String humidity="150";
 String weather="Sunny";
+String detail="";
 String mode="AUTO";
+//-----------MQTT functions---------------------------
+
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String weatherDetails = "";
+  for (int i = 0; i < length; i++) {
+    weatherDetails += (char)payload[i];
+  }
+    temp = getValue(weatherDetails, ',', 0);
+    humidity = getValue(weatherDetails, ',', 1);
+    weather = getValue(weatherDetails, ',', 2);
+    detail = getValue(weatherDetails, ',', 3);
+    city_nodered = getValue(weatherDetails, ',', 4);
+    country_nodered=getValue(weatherDetails, ',', 5);
+    Serial.println("temp"+temp);
+    //Serial.println("location"+location);
+    char msgr[30]="got it";
+    Serial.print("Publish message: ");
+    Serial.println(msgr);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+  //    client.publish(outTopic, "hello world");
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 //------------------------------------------
 void XML()
 {
@@ -67,6 +141,11 @@ void location() { //Handler for the body path
       }
     city=server.arg("city");
     country=server.arg("country");
+    String locdetails="{\"city\":\""+city+"\",\"country\":\""+country+"\"}" ;
+    Serial.println(locdetails);
+    locdetails.toCharArray(msg,locdetails.length()+1);
+    Serial.println(msg);
+    client.publish(outTopic,msg);
     Serial.println("Location set to"+country+" "+city);
     server.send(200,"text/plain","Location set to"+country+" "+city);
     return;
@@ -76,6 +155,7 @@ void location() { //Handler for the body path
 void setup()
 {
   Serial.begin(115200);
+  pinMode(BUILTIN_LED, OUTPUT);   
   WiFi.begin(ssid, password);
   while(WiFi.status()!=WL_CONNECTED){delay(500);Serial.print(".");}
   Serial.println();
@@ -85,9 +165,17 @@ void setup()
   server.on("/xml",XML);
   server.on("/location", location);
   server.begin();
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 //=================================================================
 void loop()
 {
   server.handleClient();
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 }
