44ebb585c6fc5f378458739ed5dd5a8cf113184e
Yasod <yasodginige98@gmail.com>
2021-07-01 08:28:41 +0530

    Update WebServer_final.ino
    DeepSleep functionality added for the Auto Mode.

--- WebServer_final/WebServer_final.ino
+++ WebServer_final/WebServer_final.ino
@@ -6,21 +6,33 @@
 #include <PubSubClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <TimeLib.h>
 #include "webpage.h";
+//#include <bits/stdc++.h>
 
 
 #define Valve D4
 
-const long utcOffsetInSeconds = 19800;
+using namespace std;
+String utcOffsetInSeconds_s;
+int utcOffsetInSeconds=0;
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+NTPClient timeClient(ntpUDP, "pool.ntp.org",0);
+int H;
+int M;
+int S;
+int Time;
+
+int Starting_time;
+int Current_time;
+
 
 //------------------------------------------
 ESP8266WebServer server(80);
 WiFiClient espClient;
 PubSubClient client(espClient);
-const char* ssid = "RANSIKA";
-const char* password = "RWIFI1234";
+const char* ssid = "SLT-4G-3F4C";
+const char* password = "5HJ39M13JDM";
 
 const char* mqtt_server = "test.mosquitto.org";
 const char* outTopic = "ENTC/EN2560/out/180241M";
@@ -71,6 +83,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
     weather = getValue(weatherDetails, ',', 2);
     city_nodered = getValue(weatherDetails, ',', 3);
     country_nodered=getValue(weatherDetails, ',', 4);
+    utcOffsetInSeconds_s=getValue(weatherDetails, ',', 5);
+    utcOffsetInSeconds=utcOffsetInSeconds_s.toInt();
+    
+    
     Serial.println("temp"+temp);
     //Serial.println("location"+location);
     char msgr[30]="got it";
@@ -142,7 +158,7 @@ void method(){
 }
 
 void Auto(String temp,String humidity, String weather, int H){
-Serial.println("Auto");
+//Serial.println("Auto");
 String mode="AUTO";
 if (weather=="rain" ||weather=="shower rain"|| weather=="thunderstorm"){
     //we don't have to water
@@ -150,20 +166,21 @@ if (weather=="rain" ||weather=="shower rain"|| weather=="thunderstorm"){
   }
   else{
     
-    unsigned long On_time=temp.toFloat()/10*6e4 + (100-humidity.toFloat())/10*6e4;
+    unsigned long On_time=temp.toFloat()/10*6e4 + (100-humidity.toFloat())/50*6e4;
     //int h=H.toInt();
-    if (9<H<10 || 21<H<22){
+    Serial.println(H);
+    if (9<H<10 || 16<H<17){
       if (flag){
         
         Serial.println(On_time);
         digitalWrite(Valve,HIGH);
         Serial.println("Auto Watering Happening");
-        delay(On_time/20);
+        delay(On_time/4);
         digitalWrite(Valve,LOW);
         Serial.println("Auto Watering stopped****************************************");
-        delay(10000);
+        delay(1000);
         flag=false;
-        //ESP.deepSleep(3600e6);
+        ESP.deepSleep(3600e6);
       }
       
     }
@@ -175,13 +192,13 @@ if (weather=="rain" ||weather=="shower rain"|| weather=="thunderstorm"){
 }
 
 void Manual(){
-digitalWrite(Valve,HIGH);
-Serial.println("Manual Watering Happening");
-delay(30e3);
-Serial.println("Manual Watering stopped****************************************");
-digitalWrite(Valve,LOW);
-server.send(200,"text/plain","Manual watering happened");
-//ESP.deepSleep(3600e6);
+  digitalWrite(Valve,HIGH);
+  Serial.println("Manual Watering Happening");
+  delay(30e3);
+  Serial.println("Manual Watering stopped****************************************");
+  digitalWrite(Valve,LOW);
+  server.send(200,"text/plain","Manual watering happened");
+  //ESP.deepSleep(3600e6);
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 void location() { //Handler for the body path
@@ -228,25 +245,48 @@ void setup()
   server.on("/location", location);
   server.begin();
 
+  Starting_time=millis();
+  
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
 //=================================================================
 void loop()
 {
+  
   server.handleClient();
-
+  
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+  
+  
   timeClient.update();                            // This function gets the local time of the NodeMCU. This should run frequently
-    int H=timeClient.getHours();
-    int M=timeClient.getMinutes();
-    int S=timeClient.getSeconds();
-if (mode=="AUTO"){
-Auto(temp,humidity,weather,H);
-}
+    H=timeClient.getHours();
+    M=timeClient.getMinutes();
+    S=timeClient.getSeconds();
+    Time=H*3600+M*60+S;
+    Time=Time+utcOffsetInSeconds;
+    
+    H= floor(Time/3600);
+    Time=Time%3600;
+    M=floor(Time/60);
+    S=Time%60;
+
+    Serial.print(H);
+    Serial.print(" : ");
+    Serial.print(M);
+    Serial.print(" : ");
+    Serial.println(S);
+    
+  if (mode=="AUTO"){
+    Current_time=millis();
+    if (Current_time>Starting_time+120e3){         // Auto function deepsleep
+      ESP.deepSleep(300e6);
+    }
+    Auto(temp,humidity,weather,H);
+  }
 
 
 }
